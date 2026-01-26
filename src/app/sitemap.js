5fb01baa58106e5b2b182401a9d1a19cc0112b15
Ayushhdd <ayushmaan11dhawan@gmail.com>
2026-01-26 16:31:17 +0530

    feat: add sitemap and robots.txt

---
+++ src/app/sitemap.js
@@ -0,0 +1,36 @@
+export default function sitemap() {
+    const baseUrl = "https://divishamakeovers.com";
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/services/bridal`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/services/engagement&reception`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/services/nails`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/services/party`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+    ];
+}
